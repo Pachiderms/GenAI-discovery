be37ccb3c89ab3e207811bbb04c6baeb9fa95a8c
pachiderms <zizi.telvin@gmail.com>
2026-04-10 15:13:00 +0200

    adding audio

--- rag/rag_local.py
+++ rag/rag_local.py
@@ -5,6 +5,31 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.llms import Ollama
 from langchain_classic.chains import RetrievalQA
 import os
+import numpy as np
+import wave
+import struct
+import pyaudio
+
+chunk = 1024
+format = pyaudio.paInt16
+channels = 1
+rate = 44100
+sec = 5
+filename = "test.wav"
+listen_time = int(rate / chunk * sec)
+threshold = 3.5
+close_timer = 2
+
+pa = pyaudio.PyAudio()
+
+stream = pa.open(
+    format=format,
+    channels=channels,
+    rate=rate,
+    frames_per_buffer=chunk,
+    input=True
+)
+
 
 DB_DIR = "./chroma_db"
 FILE_PATH = "./rag/rag_test.pdf"
@@ -48,5 +73,27 @@ if __name__ == "__main__":
         question = input("Posez votre question (ou tapez 'exit' pour quitter) : ")
         if question.lower() == 'exit':
             break
+        elif question.lower() == 'listen':
+            print("Écoute en cours...")
+            while True:
+                frames = []
+                data = stream.read(chunk)
+                data_int = struct.unpack(str(2*chunk) + 'B', data)
+                avg = sum(data_int) / len(data_int)
+                print(avg)
+                frames.append(data)
+                if avg < threshold:
+                    break
+            stream.stop_stream()
+            stream.close()
+            pa.terminate()
+            print("Enregistrement terminé.")
+            # Sauvegarder l'audio dans un fichier
+            with wave.open(filename, 'wb') as wf:
+                wf.setnchannels(channels)
+                wf.setsampwidth(pa.get_sample_size(format))
+                wf.setframerate(rate)
+                wf.writeframes(b''.join(frames))
+            print(f"Audio sauvegardé dans {filename}")
         response = qa_chain.invoke(question)
         print(f'Reponse:\n {response["result"]}')
